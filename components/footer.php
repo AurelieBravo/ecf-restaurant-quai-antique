47a54bdeeb61bb44f09b00b1154eaab64bb2ce6a
AurelieBravo <aurelie.geoffray@gmail.com>
2023-02-22 05:49:19 +0100

    first commit

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.com</a>
+
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Horaire d'ouverture</h3>
+         <p>du Mardi au Dimanche Midi</p>
+         <p>12h00 à 14h00</p>
+         <p>et du Vendredi au samedi soir</p>
+         <p>19h00 à 22h00</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>adresse</h3>
+         <a href="#">mumbai, india - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
